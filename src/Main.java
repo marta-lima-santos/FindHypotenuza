f616efe1cfec47ac7a03f0163c7ba0b38dcb706a
Marta Lima <121465689+marta-lima-santos@users.noreply.github.com>
2023-06-03 15:22:26 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        double x;
+        double y;
+        double z; //Hypotenuse result
+        Scanner newz = new Scanner(System.in);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        System.out.println("Enter side x: ");
+        x = newz.nextDouble();
+        System.out.println("Enter side y: ");
+        y = newz.nextDouble();
+        z = Math.sqrt((x*x)+(y*y)); //the formula
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("The hypotenuse is: "+z);
         }
     }
-}
